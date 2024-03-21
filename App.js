f8bc6cabf2c20a06aadf745fafdd1b0e9533d956
hoangthanhtung2001 <99401621+hoangthanhtung2001@users.noreply.github.com>
2024-03-21 16:53:19 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './Components/Header';
+import Main from './Components/Main/Main';
+import Navigation from './Components/Navigation/Navigation';
+function App() {
+
+  return (
+    <div className="App">
+      <Header/>
+      <Navigation/>
+      <Main/>
+    </div>
+  );
+}
+
+export default App;
